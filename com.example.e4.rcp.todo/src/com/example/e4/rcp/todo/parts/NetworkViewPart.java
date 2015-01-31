0fd6348f315addfc79d6302db1bafe71ceb3eac7
leaobr <mv2axx@gmail.com>
2015-01-31 22:10:43 +0000

    Adding a network perspective with a simple part containing the filesystem tree (to be modified to show the current network nodes)

---
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/NetworkViewPart.java
@@ -0,0 +1,135 @@
+package com.example.e4.rcp.todo.parts;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+public class NetworkViewPart implements ITreeContentProvider {
+	private TreeViewer viewer;
+	private Image image;
+
+	@PostConstruct
+	public void createControls(Composite parent) {
+		createImage();
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setInput(File.listRoots());
+	}
+
+	private void createImage() {
+		Bundle bundle = FrameworkUtil.getBundle(ViewLabelProvider.class);
+		URL url = FileLocator.find(bundle, new Path("icons/computer.png"), null);
+		ImageDescriptor imageDcr = ImageDescriptor.createFromURL(url);
+		this.image = imageDcr.createImage();
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getParent(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Focus
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+
+	@PreDestroy
+	public void dispose() {
+		image.dispose();
+	}
+
+	class ViewContentProvider implements ITreeContentProvider {
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			return (File[]) inputElement;
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			File file = (File) parentElement;
+			return file.listFiles();
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			File file = (File) element;
+			return file.getParentFile();
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			File file = (File) element;
+			if (file.isDirectory()) {
+				return true;
+			}
+			return false;
+		}
+
+	}
+
+	class ViewLabelProvider extends LabelProvider {
+		@Override
+		public String getText(Object element) {
+			File file = (File) element;
+			String name = file.getName();
+			return name.isEmpty() ? file.getPath() : name;
+		}
+
+		public Image getImage(Object element) {
+			File file = (File) element;
+			if (file.isDirectory()) {
+				return image;
+			}
+			return null;
+		}
+	}
+}
