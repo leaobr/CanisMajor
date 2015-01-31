d10bd435424c9004bf15686860bc1693a7dd59ef
leaobr <mv2axx@gmail.com>
2015-01-31 22:16:53 +0000

    Disabling the password dialog from the application life cycle startup

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/lifecycle/Manager.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/lifecycle/Manager.java
@@ -6,15 +6,10 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
-import org.osgi.service.prefs.BackingStoreException;
-
-import com.example.e4.rcp.todo.dialogs.PasswordDialog;
 
 public class Manager {
 
@@ -28,32 +23,6 @@ public class Manager {
 	public void postContextCreate(@Preference IEclipsePreferences prefs,
 			IApplicationContext appContext, Display display) {
 
-		final Shell shell = new Shell(SWT.TOOL | SWT.NO_TRIM);
-		PasswordDialog dialog = new PasswordDialog(shell);
-		if (user != null) {
-			dialog.setUser(user);
-		}
-
-		// close the static splash screen
-		appContext.applicationRunning();
-
-		// position the shell
-		 setLocation(display, shell);
-		
-		// open the dialog
-		if (dialog.open() != Window.OK) {
-			// close the application
-			System.exit(-1);
-		} else {
-			// Store the user values in the preferences
-			prefs.put("user", dialog.getUser());
-			try {
-				prefs.flush();
-			} catch (BackingStoreException e) {
-				e.printStackTrace();
-			}
-		}
-		
 	}
 
 	private void setLocation(Display display, Shell shell) {
