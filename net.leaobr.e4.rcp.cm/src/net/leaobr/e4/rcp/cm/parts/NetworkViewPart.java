bf04c6af5f793cfef1c1aa54c94b989abc5a7048
leaobr <mv2axx@gmail.com>
2015-02-04 16:21:09 +0000

    Let's not use a third party parser for executables such as nmap, there are too many issues involved with that approach. The aim now is to parse whatever executable manually and extract what is needed. This revision modifies the application model and the RCP to allow the user to specify where nmap lives.

--- net.leaobr.e4.rcp.cm/src/net/leaobr/e4/rcp/cm/parts/NetworkViewPart.java
+++ net.leaobr.e4.rcp.cm/src/net/leaobr/e4/rcp/cm/parts/NetworkViewPart.java
@@ -1,8 +1,6 @@
 package net.leaobr.e4.rcp.cm.parts;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,47 +8,74 @@ import javax.annotation.PreDestroy;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.nmap4j.Nmap4j;
-import org.nmap4j.core.nmap.NMapExecutionException;
-import org.nmap4j.core.nmap.NMapInitializationException;
-import org.nmap4j.data.NMapRun;
-import org.nmap4j.data.host.Hostnames;
-import org.nmap4j.data.nmaprun.Host;
-import org.nmap4j.data.nmaprun.hostnames.Hostname;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Text;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 public class NetworkViewPart implements ITreeContentProvider {
 	private TreeViewer viewer;
 	private Image image;
-	private Nmap4j nmap4j;
-	private NMapRun nmapRun;
+	private Text searchField;
+	Button searchBtn;
 
 	@PostConstruct
 	public void createControls(Composite parent) {
 		createImage();
-		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		nmap4j = new Nmap4j("/usr/local");
-		nmap4j.addFlags("-sn -PR");
-		nmap4j.includeHosts("192.168.1.0/24");
-		try {
-			nmap4j.execute();
-		} catch (NMapInitializationException e) {
-			e.printStackTrace();
-		} catch (NMapExecutionException e) {
-			e.printStackTrace();
-		}
-		viewer.setContentProvider(new ViewContentProvider());
+		FillLayout fillLayout = new FillLayout(SWT.VERTICAL);
+		fillLayout.spacing = IDialogConstants.VERTICAL_MARGIN;
+		parent.setLayout(fillLayout);
+		Composite outer = new Composite(parent, SWT.NONE);
+		FormLayout formLayout = new FormLayout();
+		outer.setLayout(formLayout);
+
+		Composite innerTop = new Composite(outer, SWT.NONE);
+		innerTop.setLayout(new GridLayout(2, false));
+		innerTop.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+		searchField = new Text(innerTop, SWT.SINGLE | SWT.BORDER | SWT.CANCEL);
+		searchField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		searchBtn = new Button(innerTop, SWT.NONE);
+		searchBtn.setText("...");
+		searchBtn.addSelectionListener(new SearchListener(parent));
+		FormData fData = new FormData();
+		fData = new FormData();
+		fData.top = new FormAttachment(0);
+		fData.bottom = new FormAttachment(0, 35);
+		fData.right = new FormAttachment(100);
+		fData.left = new FormAttachment(0);
+		innerTop.setLayoutData(fData);
+
+		Composite innerBottom = new Composite(outer, SWT.VERTICAL);
+		innerBottom.setLayout(new FillLayout(SWT.HORIZONTAL));
+		viewer = new TreeViewer(innerBottom, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		/*viewer.setContentProvider(new ViewContentProvider());
 		viewer.setLabelProvider(new ViewLabelProvider());
-		viewer.setInput(new Host());
+		viewer.setInput(new Host());*/
+		//innerBottom.setLayout(fillLayout);
+		fData = new FormData();
+		fData.top = new FormAttachment(innerTop);
+		fData.left = new FormAttachment(0);
+		fData.right = new FormAttachment(100);
+		fData.bottom = new FormAttachment(100);
+		innerBottom.setLayoutData(fData);
 	}
 
 	private void createImage() {
@@ -71,7 +96,7 @@ public class NetworkViewPart implements ITreeContentProvider {
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
-		return nmapRun.getHosts().toArray();
+		return null; //TODO
 	}
 
 	@Override
@@ -104,12 +129,7 @@ public class NetworkViewPart implements ITreeContentProvider {
 
 		@Override
 		public Object[] getElements(Object inputElement) {
-			List<Object> children = new ArrayList<Object>();
-			if (!nmap4j.hasError()) {
-				nmapRun = nmap4j.getResult();
-				children.addAll(nmapRun.getHosts());
-			}//TODO see what happens when nmap4j has errors
-			return children.toArray();
+			return null; //TODO
 		}
 
 		@Override
@@ -132,14 +152,34 @@ public class NetworkViewPart implements ITreeContentProvider {
 	class ViewLabelProvider extends LabelProvider {
 		@Override
 		public String getText(Object element) {
-			Host host = (Host) element;
-			Hostnames hostNames = host.getHostnames();
-			Hostname hostName = hostNames != null ? hostNames.getHostname() : null;
-			return hostName == null ? host.getAddresses().get(0).getAddr() : hostName.getName();
+			return super.getText(element); //TODO
 		}
 
 		public Image getImage(Object element) {
 			return image;
 		}
+	}
+
+	class SearchListener implements SelectionListener {
+
+		private final Composite parent;
+
+		public SearchListener(Composite parent) {
+			this.parent = parent;
+		}
+
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			FileDialog fd = new FileDialog(parent.getShell(), SWT.OPEN);
+			fd.setText("Select File Path");
+			fd.setFilterNames(new String[] { "All Files (*.*)" });
+			fd.setFilterExtensions(new String[] { "*.*" });
+			fd.open();
+		}
+
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {
+		}
+
 	}
 }
