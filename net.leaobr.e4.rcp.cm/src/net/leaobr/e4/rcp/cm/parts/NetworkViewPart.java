7d952ba063d49636c56ae10f26e2b2f9459ac87b
leaobr <mv2axx@gmail.com>
2015-02-05 00:34:12 +0000

    Readding nma4j to be used as the nmap parser but this time as an integrated bundle as part of the whole project. Hopefully this way it feels more native to the app and erros can be handled more easily. This revision also brings minor changes to the RCP.

--- net.leaobr.e4.rcp.cm/src/net/leaobr/e4/rcp/cm/parts/NetworkViewPart.java
+++ net.leaobr.e4.rcp.cm/src/net/leaobr/e4/rcp/cm/parts/NetworkViewPart.java
@@ -1,6 +1,8 @@
 package net.leaobr.e4.rcp.cm.parts;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -28,14 +30,22 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Text;
+import org.nmap4j.Nmap4j;
+import org.nmap4j.data.NMapRun;
+import org.nmap4j.data.host.Hostnames;
+import org.nmap4j.data.nmaprun.Host;
+import org.nmap4j.data.nmaprun.hostnames.Hostname;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 public class NetworkViewPart implements ITreeContentProvider {
-	private TreeViewer viewer;
+	public static TreeViewer viewer;
 	private Image image;
-	private Text searchField;
-	Button searchBtn;
+	public static Text searchField;
+	private Button searchBtn;
+	private String nmapPath;
+	public static Nmap4j nmap4j;
+	public static NMapRun nmapRun;
 
 	@PostConstruct
 	public void createControls(Composite parent) {
@@ -54,11 +64,11 @@ public class NetworkViewPart implements ITreeContentProvider {
 		searchField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		searchBtn = new Button(innerTop, SWT.NONE);
 		searchBtn.setText("...");
-		searchBtn.addSelectionListener(new SearchListener(parent));
+		searchBtn.addSelectionListener(new NmapPathInitializer(parent));
 		FormData fData = new FormData();
 		fData = new FormData();
 		fData.top = new FormAttachment(0);
-		fData.bottom = new FormAttachment(0, 35);
+		fData.bottom = new FormAttachment(0, 50);
 		fData.right = new FormAttachment(100);
 		fData.left = new FormAttachment(0);
 		innerTop.setLayoutData(fData);
@@ -66,9 +76,9 @@ public class NetworkViewPart implements ITreeContentProvider {
 		Composite innerBottom = new Composite(outer, SWT.VERTICAL);
 		innerBottom.setLayout(new FillLayout(SWT.HORIZONTAL));
 		viewer = new TreeViewer(innerBottom, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		/*viewer.setContentProvider(new ViewContentProvider());
+		viewer.setContentProvider(new ViewContentProvider());
 		viewer.setLabelProvider(new ViewLabelProvider());
-		viewer.setInput(new Host());*/
+		viewer.setInput(new Host());
 		//innerBottom.setLayout(fillLayout);
 		fData = new FormData();
 		fData.top = new FormAttachment(innerTop);
@@ -96,7 +106,7 @@ public class NetworkViewPart implements ITreeContentProvider {
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
-		return null; //TODO
+		return nmapRun.getHosts().toArray();
 	}
 
 	@Override
@@ -129,7 +139,12 @@ public class NetworkViewPart implements ITreeContentProvider {
 
 		@Override
 		public Object[] getElements(Object inputElement) {
-			return null; //TODO
+			List<Object> children = new ArrayList<Object>();
+			if (nmap4j != null && !nmap4j.hasError()) {
+				nmapRun = nmap4j.getResult();
+				children.addAll(nmapRun.getHosts());
+			}//TODO see what happens when nmap4j has errors
+			return children.toArray();
 		}
 
 		@Override
@@ -152,7 +167,10 @@ public class NetworkViewPart implements ITreeContentProvider {
 	class ViewLabelProvider extends LabelProvider {
 		@Override
 		public String getText(Object element) {
-			return super.getText(element); //TODO
+			Host host = (Host) element;
+			Hostnames hostNames = host.getHostnames();
+			Hostname hostName = hostNames != null ? hostNames.getHostname() : null;
+			return hostName == null ? host.getAddresses().get(0).getAddr() : hostName.getName();
 		}
 
 		public Image getImage(Object element) {
@@ -160,11 +178,11 @@ public class NetworkViewPart implements ITreeContentProvider {
 		}
 	}
 
-	class SearchListener implements SelectionListener {
+	class NmapPathInitializer implements SelectionListener {
 
 		private final Composite parent;
 
-		public SearchListener(Composite parent) {
+		public NmapPathInitializer(Composite parent) {
 			this.parent = parent;
 		}
 
@@ -174,7 +192,8 @@ public class NetworkViewPart implements ITreeContentProvider {
 			fd.setText("Select File Path");
 			fd.setFilterNames(new String[] { "All Files (*.*)" });
 			fd.setFilterExtensions(new String[] { "*.*" });
-			fd.open();
+			nmapPath = fd.open();
+			searchField.setText(nmapPath);
 		}
 
 		@Override
