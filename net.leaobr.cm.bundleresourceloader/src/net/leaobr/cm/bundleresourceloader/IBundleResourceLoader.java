38efc514ae601a76003a03c77169c8dda926717a
leaobr <mv2axx@gmail.com>
2015-02-08 23:23:53 +0000

    Removing unused bundles

--- net.leaobr.cm.bundleresourceloader/src/net/leaobr/cm/bundleresourceloader/IBundleResourceLoader.java
+++
@@ -1,7 +0,0 @@
-package net.leaobr.cm.bundleresourceloader;
-
-import org.eclipse.swt.graphics.Image;
-
-public interface IBundleResourceLoader {
-	public Image loadImage(Class<?> clazz, String path);
-}
