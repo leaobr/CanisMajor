7b4461b91e42fb06616feb6425cfe23217d29d78
Tiago Leao <mv2axx@gmail.com>
2015-02-18 15:33:42 +0000

    Removing duplicated code that initializes the console. The correct one is under parts/ConsoleView as it can only be started up after the workbench is ready by using the earlyStartup extension point.

--- canisMajor/net.leaobr.cm.application/src/net/leaobr/rcp/cm/lifecycle/Manager.java
+++ canisMajor/net.leaobr.cm.application/src/net/leaobr/rcp/cm/lifecycle/Manager.java
@@ -1,16 +1,10 @@
 package net.leaobr.rcp.cm.lifecycle;
 
-import java.io.PrintStream;
-
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.console.ConsolePlugin;
-import org.eclipse.ui.console.IConsole;
-import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
 
 @SuppressWarnings("restriction")
 public class Manager {
@@ -18,15 +12,5 @@ public class Manager {
 	@PostContextCreate
 	public void postContextCreate(@Preference IEclipsePreferences prefs,
 			IApplicationContext appContext, Display display) {
-		initializeConsole();
-	}
-
-	private void initializeConsole() {
-		final MessageConsole console = new MessageConsole("System Output", null);
-		console.setWaterMarks(500000, 1000000);
-		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { console });
-		MessageConsoleStream stream = console.newMessageStream();
-		System.setOut(new PrintStream(stream));
-		System.setErr(new PrintStream(stream));
 	}
 }
